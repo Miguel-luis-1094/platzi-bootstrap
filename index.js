c493f07385b0625e056a5e64867c2301c7bf685f
miguel valencia <ingindumiguel@gmail.com>
2020-06-07 06:42:52 -0500

    proyecto bootstrap

---
+++ index.js
@@ -0,0 +1,5 @@
+/* esta funcion se hace para que al poner el cursor en el init se active y aparezca un mensaje, esto gracias a jquery que viene con bootstrap */
+
+$(function() {
+    $('[data-toogle="tooltip"]').tooltip()
+})
